52f690d103db996192b2c73916ed7227da1bb63d
Mark Nyon <mark.nyon@runads.com>
2016-10-11 20:03:00 -0400

    Default index file for project

--- js/index.js
+++ js/index.js
@@ -1,21 +1,32 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var Person = function() {
-    var name = 'Derek Zoolander';
-    var imageUrl = 'http://uifaces.com/assets/static/images/zoolander.jpg';
-    var job = 'Male model';
+var Person = function(props) {
     return (
         <div className="person">
-            <div className="person-name">{name}</div>
-            <img className="person-img" src={imageUrl} />
+            <div className="person-name">{props.name}</div>
+            <img className="person-img" src={props.imageUrl} />
             <div className="person-job">
-                {job}
+                {props.job}
             </div>
         </div>
     );
 };
 
+var PersonList = function() {
+    return (
+        <div className="person-list">
+            <Person name="Derek Zoolander"
+                    imageUrl="http://uifaces.com/assets/static/images/zoolander.jpg"
+                    job="Male model" />
+            <Person name="Donald Knuth"
+                    imageUrl="http://www-cs-faculty.stanford.edu/~uno/don.gif"
+                    job="Clever chap" />
+        </div>
+    );
+};
+
+
 document.addEventListener('DOMContentLoaded', function() {
-    ReactDOM.render(<Person />, document.getElementById('app'));
+    ReactDOM.render(<PersonList />, document.getElementById('app'));
 });
